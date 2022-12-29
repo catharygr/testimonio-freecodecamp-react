107b36eb25db536d14b462ffaaab1b0fd696440e
cathy <cathy@bubulazi.com>
2022-12-29 13:54:38 +0000

    Usando dangerouslySetInnerHTML para poner letras en bold

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Testimonio from './componentes/Testimonio';
 import './App.css';
 
 export default function App() {
+ 
   return (
     <div className="App">
         <div className='contenedor-principal'>
@@ -13,21 +14,21 @@ export default function App() {
           imagen='sasa'
           cargo='Ingeniero de Software'
           empresa='Apple'
-          testimonio='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.'/>
+          testimonio='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de <b>lecciones gratuitas en freeCodeCamp</b>.Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.'/>
         <Testimonio
           nombre='Clau'
           pais='España'
           imagen='clau'
           cargo='Graduada en diseño en 3D'
           empresa='ChatDesk'
-          testimonio='freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.'/>
+          testimonio='<b>freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software.</b> El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.'/>
         <Testimonio 
           nombre='Caty'
           pais='Cuba'
           imagen='popo'
           cargo='Desarrolladora frontend'
           empresa='Spotify'
-          testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'/>
+          testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en <b>freeCodeCamp me dio las habilidades </b> y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'/>
       </div>
     </div>
 
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -3,12 +3,15 @@ import React from 'react';
 import '../hojas-de-estilo/testimonio.css';
 
 export default function Testimonio(props) {
+
+
+
   return (
     <div className='contenedor-testimonio'>
       <img 
         className='imagen-testimonio'
         src={require(`../imagenes/${props.imagen}.jpeg`)}
-        alt='Foto de Sasa'/>
+        alt='Foto de '/>
       <div className='contenedor-texto-testimonio'>
         <p className='nombre-testimonio'>
           <strong>{props.nombre}</strong> en {props.pais}
@@ -16,7 +19,8 @@ export default function Testimonio(props) {
         <p className='cargo-testimonio'>
           {props.cargo} en <strong>{props.empresa}</strong>
         </p>
-        <p className='texto-testimonio'>"{props.testimonio}"</p>
+        <p className='texto-testimonio' dangerouslySetInnerHTML={{__html: props.testimonio}} ></p>
+
       </div>
     </div>
   );
